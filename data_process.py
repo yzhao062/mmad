9607886da1bd15216f47cc4a753dad24d57b1189
Yue Zhao <zhaoy@cmu.edu>
2020-10-10 14:23:39 -0400

    add anomaly label

--- data_process.py
+++ data_process.py
@@ -16,6 +16,9 @@ train_df_source = pd.read_csv(os.path.join("CrisisMMD_v2.0", "crisismmd_dataspli
 train_df_aligned = train_df_source[train_df_source.label_text_image=="Positive"]
 train_df_aligned.reset_index(drop=True, inplace=True)
 
+train_df_aligned['anomaly'] = 0
+train_df_aligned['anomaly'][train_df_aligned['label']=="infrastructure_and_utility_damage"]=1
+
 train_df_aligned.to_excel("train_cleaned.xlsx", index=False)
 
 #%%
@@ -24,6 +27,9 @@ test_df_source = pd.read_csv(os.path.join("CrisisMMD_v2.0", "crisismmd_datasplit
 # only keep the ones that image & text label align
 test_df_aligned = test_df_source[test_df_source.label_text_image=="Positive"]
 test_df_aligned.reset_index(drop=True, inplace=True)
+
+test_df_aligned['anomaly'] = 0
+test_df_aligned['anomaly'][test_df_aligned['label']=="infrastructure_and_utility_damage"]=1
 
 test_df_aligned.to_excel("test_cleaned.xlsx", index=False)
 
