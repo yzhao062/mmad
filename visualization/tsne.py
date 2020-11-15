b393f0002bc69c4e57493299e27ffcb4002762ba
xiyanghu <xiyanghu@outlook.com>
2020-11-15 14:51:05 -0500

    update multimodal visualization

--- visualization/tsne.py
+++ visualization/tsne.py
@@ -8,18 +8,28 @@ import matplotlib.patheffects as PathEffects
 import matplotlib.patches as mpatches
 import seaborn as sns
 import pandas as pd
-
+from sklearn.preprocessing import StandardScaler
 image_emb = np.load('../unimodality/image/train_image_embedding.npy')
 text_w2v_emb = np.load('../unimodality/language/word2vec.npy')
 text_bert_emb = np.load('../unimodality/language/bert.npy')
 
-image_tsne = TSNE(n_components=2).fit_transform(image_emb)
-text_w2v_tsne = TSNE(n_components=2).fit_transform(text_w2v_emb)
-text_bert_tsne = TSNE(n_components=2).fit_transform(text_bert_emb)
+mulmodal_wen_emb = np.load('../multimodality/baseline_wen_embeding.npy')
+mulmodal_con_emb = np.load('../multimodality/input_concat.npy')
+mulmodal_vae_emb = np.load('../multimodality/vae_joint_representation.npy')
+
+image_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(image_emb))
+text_w2v_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(text_w2v_emb))
+text_bert_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(text_bert_emb))
+
+mulmodal_wen_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(mulmodal_wen_emb))
+mulmodal_con_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(mulmodal_con_emb))
+mulmodal_vae_tsne = TSNE(n_components=2).fit_transform(StandardScaler().fit_transform(mulmodal_vae_emb))
 
 train_df = pd.read_excel("../train_cleaned.xlsx")
 label_image = train_df["label_image"].values
 label_text = train_df["label_text"].values
+
+label = train_df["anomaly"].values
 
 def scatter(x, label):
     # We choose a color palette with seaborn.
@@ -38,7 +48,8 @@ def scatter(x, label):
     plt.xlim(-25, 25)
     plt.ylim(-25, 25)
     
-    ls = [mpatches.Patch(color=palette[i],label=str(i)) for i in range(8)]
+    #ls = [mpatches.Patch(color=palette[i],label=str(i)) for i in range(8)]
+    ls = [mpatches.Patch(color=palette[i],label=str(i)) for i in range(2)]
     ax.legend(handles=ls)
     ax.axis('off')
     ax.axis('tight')
@@ -56,11 +67,20 @@ def scatter(x, label):
 
     return f, ax, sc, txts
 
-scatter(image_tsne, label_image)
+scatter(image_tsne, label)
 plt.savefig('image_tsn.png', dpi=120)
 
-scatter(text_w2v_tsne, label_text)
+scatter(text_w2v_tsne, label)
 plt.savefig('text_w2v_tsne.png', dpi=120)
 
-scatter(text_bert_tsne, label_text)
+scatter(text_bert_tsne, label)
 plt.savefig('text_bert_tsne.png', dpi=120)
+
+scatter(mulmodal_wen_tsne, label)
+plt.savefig('mulmodal_wen_tsne.png', dpi=120)
+
+scatter(mulmodal_con_tsne, label)
+plt.savefig('mulmodal_con_tsne.png', dpi=120)
+
+scatter(mulmodal_vae_tsne, label)
+plt.savefig('mulmodal_vae_tsne.png', dpi=120)
